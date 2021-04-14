23831b0f203c895eb634b24afec3fca1cd0ec035
Carlos Sala Samper <c.sala@geekshubs.com>
2021-04-14 08:25:21 +0200

    Some comments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
+let mongoClientInstance; // Usable MongoDB instance
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -8,11 +9,15 @@ app.use(express.urlencoded({ extended: false }));
 MongoClient.connect(
   'mongodb://mongo:27017/docker-node-mongo',
   (err, client) => {
+    mongoClientInstance = client;
     console.log('MongoDB Connected');
   })
 
+/**
+ * @description http://localhost
+ */
 app.get('/', (req, res) => {
-  res.json({ message: "Hello world" })
+  res.json({ message: "Hello world!" })
 });
 
 const PORT = 3000;
