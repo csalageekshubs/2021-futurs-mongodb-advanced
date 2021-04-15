52078d37ea3e92ed74a77515a6ec17d36a1174c2
Carlos Sala Samper <c.sala@geekshubs.com>
2021-04-15 08:34:50 +0200

    Deleted promises code

--- index.js
+++ index.js
@@ -21,61 +21,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/kata1', (req, res) => {
-  const session = mongoClientInstance.startSession();
-  const transactionOptions = {
-    readPreference: 'primary',
-    readConcern: { level: 'local' },
-    writeConcern: { w: 'majority' }
-  };
-
-  session.withTransaction(() => {
-    const usersCollection = mongoClientInstance.db('local').collection('users');
-
-    usersCollection.insertOne({
-      name: req.body.name,
-      surname: req.body.surname,
-      email: req.body.email,
-      age: req.body.age
-    }, { session }
-    );
-  },
-    transactionOptions
-  )
-    .then(() => { })
-    .catch((error) => {
-      console.log("Error during transaction: ", error);
-    })
-    .then(() => {
-      session.close();
-    });
-
-  res.json({ message: "Transaction end!" })
+  res.json({ message: "Kata1 end!" })
 });
 
 app.get('/kata2', (req, res) => {
-  mongoClientInstance.db("test").collection('usersKata2').aggregate([
-    {
-      "$geoNear": {
-        "near": {
-          "type": "Point",
-          "coordinates": [
-            -3.8891601562499996,
-            40.43022363450862
-          ]
-        },
-        "maxDistance": 500 * 1609,
-        "spherical": true,
-        "distanceField": "distance",
-        "distanceMultiplier": 0.000621371
-      }
-    }
-  ]).pretty()
-    .then((result) => {
-      console.log(result);
-      res.json({ result })
-    })
+  res.json({ message: "Kata2 end!" })
 });
-
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server running...'));
