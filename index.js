9cfdbdf4e1ccb9505360ba8850a14207978cc10e
Carlos Sala Samper <c.sala@geekshubs.com>
2021-04-15 08:07:41 +0200

    Added babel to use modern sintax: async waait, import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 const MongoClient = require('mongodb').MongoClient
 let mongoClientInstance; // Usable MongoDB instance
 
@@ -19,6 +19,63 @@ MongoClient.connect(
 app.get('/', (req, res) => {
   res.json({ message: "Hello world!" })
 });
+
+app.post('/kata1', (req, res) => {
+  const session = mongoClientInstance.startSession();
+  const transactionOptions = {
+    readPreference: 'primary',
+    readConcern: { level: 'local' },
+    writeConcern: { w: 'majority' }
+  };
+
+  session.withTransaction(() => {
+    const usersCollection = mongoClientInstance.db('local').collection('users');
+
+    usersCollection.insertOne({
+      name: req.body.name,
+      surname: req.body.surname,
+      email: req.body.email,
+      age: req.body.age
+    }, { session }
+    );
+  },
+    transactionOptions
+  )
+    .then(() => { })
+    .catch((error) => {
+      console.log("Error during transaction: ", error);
+    })
+    .then(() => {
+      session.close();
+    });
+
+  res.json({ message: "Transaction end!" })
+});
+
+app.get('/kata2', (req, res) => {
+  mongoClientInstance.db("test").collection('usersKata2').aggregate([
+    {
+      "$geoNear": {
+        "near": {
+          "type": "Point",
+          "coordinates": [
+            -3.8891601562499996,
+            40.43022363450862
+          ]
+        },
+        "maxDistance": 500 * 1609,
+        "spherical": true,
+        "distanceField": "distance",
+        "distanceMultiplier": 0.000621371
+      }
+    }
+  ]).pretty()
+    .then((result) => {
+      console.log(result);
+      res.json({ result })
+    })
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log('Server running...'));
